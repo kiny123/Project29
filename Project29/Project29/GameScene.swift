a4a0b3653c5e9affd51c7405d05fe2ce6b9a0bc5
kiny123 <89192734912nik@gmail.com>
2023-04-20 22:52:28 +0300

    init

---
+++ Project29/Project29/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by nikita on 20.04.2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+     
+    }
+}
