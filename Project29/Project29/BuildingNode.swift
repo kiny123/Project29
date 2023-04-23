27d69c1d4452680e1e371b231878abd8bd9243a3
kiny123 <89192734912nik@gmail.com>
2023-04-24 01:07:28 +0300

    day 95

--- Project29/Project29/BuildingNode.swift
+++ Project29/Project29/BuildingNode.swift
@@ -8,11 +8,6 @@
 import SpriteKit
 import UIKit
 
-enum CollisionTypes: UInt32 {
-    case banana = 1
-    case building = 2
-    case player = 4
-}
 
 class BuildingNode: SKSpriteNode {
     var currentImage: UIImage!
@@ -72,6 +67,24 @@ class BuildingNode: SKSpriteNode {
         }
 
         return img
+    }
+    
+    func hit(at point: CGPoint) {
+        let convertedPoint = CGPoint(x: point.x + size.width / 2.0, y: abs(point.y - (size.height / 2.0)))
+
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let img = renderer.image { ctx in
+            currentImage.draw(at: .zero)
+
+            ctx.cgContext.addEllipse(in: CGRect(x: convertedPoint.x - 32, y: convertedPoint.y - 32, width: 64, height: 64))
+            ctx.cgContext.setBlendMode(.clear)
+            ctx.cgContext.drawPath(using: .fill)
+        }
+
+        texture = SKTexture(image: img)
+        currentImage = img
+
+        configurePhysics()
     }
     
 }
